94f1f968431b8dc9bc28511ff69561233ad0b690
Naveen kumar <126279118+iamnaveen8851@users.noreply.github.com>
2024-10-28 14:33:55 +0530

    Add files via upload

---
+++ readMe.md
@@ -0,0 +1,19 @@
+## Todo App Backend setup
+
+Packages
+
+- expressjs
+- nodemon
+- dotenv
+- bcrypt
+- jsonwebtoken or jwt
+- mongoose
+
+
+
+## Structure of todo backend 
+   - server.js
+   - .env 
+   - .gitignore
+   
+# iamnaveen8851-todo_app_backend_revision
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const connectDb = require("./config/db");
+
+// Create a server
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to Todo App Server!`);
+});
+
+app.listen(PORT, async () => {
+  try {
+    await connectDb
+    console.log(`Server listening on PORT NO. ${PORT} and DB is connected`)
+  } catch (error) {
+    console.log(error.message);
+  }
+});
